0109356f1c1362db229f5fc908176b58adc00cc1
TranHung <tph.hung123@gmail.com>
2023-08-09 13:53:05 +0700

    update app

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AppService {
-    prisma = new PrismaClient();
     async getHello() {
-        const test = await this.prisma.tbl_test.findMany({});
-        return test;
+        return 'Hello world';
     }
 }
